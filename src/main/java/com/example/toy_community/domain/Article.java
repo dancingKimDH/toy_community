60636b2d193adeeabb17be694ad3c284c99c0054
dancingKimDH <kimdonghyun0701@gmail.com>
2023-11-07 19:55:33 +0900

    domain

---
+++ src/main/java/com/example/toy_community/domain/Article.java
@@ -0,0 +1,18 @@
+package com.example.toy_community.domain;
+
+import java.time.LocalDateTime;
+
+public class Article {
+
+    private Long id;
+    private String title;
+    private String hashtag;
+    private String content;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+
+}
