151f1a3bd2011cb5686e6533f4d9743f0dd46a60
dancingKimDH <kimdonghyun0701@gmail.com>
2023-11-07 20:39:03 +0900

    jpa config

--- src/main/java/com/example/toy_community/domain/Article.java
+++ src/main/java/com/example/toy_community/domain/Article.java
@@ -1,18 +1,42 @@
 package com.example.toy_community.domain;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
 import java.time.LocalDateTime;
 
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "hashtag"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@Entity
 public class Article {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+
+    @Setter
     private String title;
-    private String hashtag;
-    private String content;
+    @Setter private String content;
 
-    private LocalDateTime createdAt;
-    private String createdBy;
-    private LocalDateTime modifiedAt;
-    private String modifiedBy;
+    @Setter private String hashtag;
 
+    @CreatedDate private LocalDateTime createdAt;
+    @CreatedBy private String createdBy;
+    @LastModifiedDate private LocalDateTime modifiedAt;
+    @LastModifiedBy private String modifiedBy;
 
 }
