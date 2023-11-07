fc3502e0978d00cfa3a06e9b6d349920b2f7d878
dancingKimDH <kimdonghyun0701@gmail.com>
2023-11-07 19:48:23 +0900

    first commit

---
+++ src/main/java/com/example/toy_community/ToyCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.example.toy_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToyCommunityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/toy_community/ToyCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toy_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
